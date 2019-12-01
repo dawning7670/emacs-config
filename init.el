f3f65b1dfe4ed9a183b649f2c4917bb50a3bb4bb
dawning <dawning7670@163.com>
2019-12-01 22:31:54 +0800

    ~

--- init.el
+++ init.el
@@ -31,7 +31,7 @@ There are two things you can do about this warning:
 ; then define packages you use
 (use-package slime)
 (use-package racket-mode)
-(use-package smartparens)
+(use-package smartparens :config (show-smartparens-mode t))
 
 
 (custom-set-variables
@@ -39,14 +39,33 @@ There are two things you can do about this warning:
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(abbrev-all-caps t)
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes (quote (idea-darkula)))
+ '(custom-safe-themes
+   (quote
+    ("947190b4f17f78c39b0ab1ea95b1e6097cc9202d55c73a702395fc817f899393" "420689cc31d01fe04b8e3adef87b8838ff52faa169e69ca4e863143ae9f3a9f9" default)))
  '(package-selected-packages
    (quote
-    (smartparens use-package racket-mode slime macrostep))))
+    (company ## idea-darkula-theme dracula-theme smartparens use-package racket-mode slime macrostep))))
 ;; Set your lisp system and, optionally, some contribs
 (setq inferior-lisp-program "/usr/local/bin/sbcl")
 (setq slime-contribs '(slime-fancy))
 (setq racket-program "/Applications/Racket v7.4/bin/racket")
 (require 'smartparens-config)
+(setq tab-always-indent 'complete)
+(add-hook 'after-init-hook 'global-company-mode)
+(defun my-racket-mode-hook ()
+	(set (make-local-variable 'company-backends)
+	     '((company-capf company-dabbrev-code)))
+(company-quickhelp-mode 0))
+(add-hook 'racket-mode-hook 'my-racket-mode-hook)
+(add-hook 'racket-mode-hook 'company-mode)
+(add-hook 'racket-repl-mode-hook 'my-racket-mode-hook)
+(add-hook 'racket-repl-mode-hook 'company-mode)
+
+
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
